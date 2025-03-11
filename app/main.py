9468ed5cc1bdfe884c1eff6e0603e87c64768de1
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-03-11 13:20:29 +0530

    refactor main function to handle user input and output command not found message

--- app/main.py
+++ app/main.py
@@ -2,11 +2,9 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-
-    # Wait for user input
-    input()
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
