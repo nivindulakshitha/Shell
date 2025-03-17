89197d30611470afe6391342730839affa3f8cb4
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-03-17 08:38:06 +0530

    refactor command handling to implement a decorator for builtin commands and enhance command processing

--- app/main.py
+++ app/main.py
@@ -1,18 +1,37 @@
+import shutil
 import sys
+builtin_commands = {}
+
+def command(func):
+    builtin_commands[func.__name__.split("_")[1]] = func
+    return func
+
+@command
+def shell_exit(args):
+    exit(int(args[0]))
+
+@command
+def shell_echo(args):
+    sys.stdout.write(" ".join(args) + "\n")
+
+@command
+def shell_type(args):
+    if args[0] in builtin_commands:
+        sys.stdout.write(f"{args[0]} is a shell builtin\n")
+    elif path := shutil.which(args[0]):
+        sys.stdout.write(f"{args[0]} is {path}\n")
+    else:
+        sys.stdout.write(f"{args[0]}: not found\n")
+
 def main():
-    while 1:
+    while True:
         sys.stdout.write("$ ")
         command = input()
-        match command:
-            case command if command.startswith("echo "):
-                sys.stdout.write(f"{command[len("echo ") :]}\n")
-            case "exit 0":
-                return 0
-            case command if command.startswith("type invalid"):
-                sys.stdout.write(f"{command[len('type ') :]}: not found\n")
-            case command if command.startswith("type "):
-                sys.stdout.write(f"{command[len('type ') :]} is a shell builtin\n")
-            case _:
-                sys.stdout.write(f"{command}: command not found\n")
+        cmd, *args = command.split()
+        if cmd in builtin_commands:
+            builtin_commands[cmd](args)
+        else:
+            sys.stdout.write(f"{cmd}: command not found\n")
+
 if __name__ == "__main__":
     main()
