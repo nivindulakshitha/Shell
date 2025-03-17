6f002d224df7ec5a4d26930b205ed7af75b430e6
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-03-17 08:41:29 +0530

    refactor command handling to replace builtin command functions with a dynamic path parsing approach

--- app/main.py
+++ app/main.py
@@ -1,37 +1,46 @@
-import shutil
 import sys
-builtin_commands = {}
-
-def command(func):
-    builtin_commands[func.__name__.split("_")[1]] = func
-    return func
-
-@command
-def shell_exit(args):
-    exit(int(args[0]))
-
-@command
-def shell_echo(args):
-    sys.stdout.write(" ".join(args) + "\n")
-
-@command
-def shell_type(args):
-    if args[0] in builtin_commands:
-        sys.stdout.write(f"{args[0]} is a shell builtin\n")
-    elif path := shutil.which(args[0]):
-        sys.stdout.write(f"{args[0]} is {path}\n")
-    else:
-        sys.stdout.write(f"{args[0]}: not found\n")
-
+import os
+PATH_SEP = os.pathsep
+PATH = os.environ["PATH"]
+BUILTINS = {"exit": "builtin", "type": "builtin", "echo": "builtin"}
+def parse_path(path):
+    path = path.split(PATH_SEP)
+    path = [current_path for current_path in path if current_path]
+    for current_path in path:
+        if os.path.exists(current_path):
+            files = (
+                file
+                for file in os.listdir(current_path)
+                if os.path.isfile(os.path.join(current_path, file))
+            )
+        for file in files:
+            if file not in BUILTINS:
+                BUILTINS[file] = os.path.join(current_path, file)
 def main():
+    parse_path(PATH)
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        cmd, *args = command.split()
-        if cmd in builtin_commands:
-            builtin_commands[cmd](args)
+        args = input().split(" ")
+        if len(args) == 0:
+            continue
         else:
-            sys.stdout.write(f"{cmd}: command not found\n")
-
+            command, *params = args
+            if command == "exit":
+                return 0
+            elif command == "echo":
+                print(*params)
+            elif command == "type":
+                arg_command = params[0]
+                if arg_command in BUILTINS:
+                    if BUILTINS[arg_command] == "builtin":
+                        print(f"{arg_command} is a shell builtin")
+                    else:
+                        print(f"{arg_command} is {BUILTINS[arg_command]}")
+                else:
+                    print(f"{arg_command}: not found")
+            elif command in BUILTINS:
+                os.system(f"{command} {' '.join(params)}")
+            else:
+                print(f"{command}: command not found")
 if __name__ == "__main__":
     main()
